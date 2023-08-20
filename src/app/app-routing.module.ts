95e1e1db3abdf75887d6ed06c849e7aa0d8e0e10
One Developer <one.developer.1999@gmail.com>
2023-08-20 10:31:42 +0530

    Added routing modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,1 +1,14 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes, PreloadAllModules } from '@angular/router';
 
+const app_routes: Routes = [
+  { path: 'auth', loadChildren: () => import('./modules/auth/auth.module').then(m => m.AuthModule) },
+  { path: '', pathMatch: 'full', redirectTo: '/auth/login' },
+  { path: '**', redirectTo: '/404' },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(app_routes, { preloadingStrategy: PreloadAllModules })],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
